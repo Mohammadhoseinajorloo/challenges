4910754a5f9028e335e930169ab1cdcf528c287e
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-05-27 03:06:02 +0330

    2Mon May 27 03:05:44 AM +0330 2024

--- oop/Exercises3.1/E4.py
+++ oop/Exercises3.1/E4.py
@@ -3,16 +3,36 @@ from picture import Picture
 from color import Color
 import sys, stddraw
 
+def histogram(data, nbins, min_val=None, max_val=None):
+    hist_vals = [0]*(nbins+1)
+    if min_val is None:
+        min_val = min(data)
+    if max_val is None:
+        max_val = max(data)
+
+    for d in data:
+        bin_number = int(nbins * ((d - min_val) / (max_val - min_val)))
+        hist_vals[bin_number] += 1
+    bin_lower_bounds = [min_val + i*(max_val - min_val)/len(hist_vals) for i in range(len(hist_vals))]
+    return hist_vals, bin_lower_bounds
+
 def main():
     pic1 = Picture(sys.argv[1])
     h = pic1.height()
-    w = pic1.weight()
-    pic2 = Picture(h, w)
+    w = pic1.width()
 
+    list_ = []
     for row in range(w):
         for col in range(h):
-            
-
+            b = pic1.get(row, col).getBlue()
+            if b not in list_:
+                list_.append(b)
+    hist_vals = histogram(list_, 3)
+    for i in hist_vals[0]:
+        stddraw.setCanvasSize(512, 512)
+        stddraw.setYscale(.5)
+        stddraw.filledRectangle(1, 2, 1.0, i)
+        stddraw.show()
 
 if __name__ == "__main__":
     main()
