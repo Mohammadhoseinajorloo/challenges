2ce90a8074f49a2f7b4b8be3489a67139b266408
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-08-18 03:32:46 +0330

    approch 3

--- leetcode/ispolindromenumber.py
+++ leetcode/ispolindromenumber.py
@@ -1,4 +1,17 @@
 
+def isPalindrome3(x: int) -> bool:
+    if x < 0:
+        return False
+
+    reversed_num = 0
+    temp = x
+
+    while temp != 0:
+        digit = temp % 10
+        reversed_num = reversed_num * 10 + digit
+        temp //= 10
+
+    return reversed_num == x
 
 def ispolidrome(x:int) -> bool:
     x = str(x)
@@ -11,4 +24,4 @@ def isPolindrome(x:int) -> bool:
     x = str(x)
     if x[::-1] != x:
         return False
-    return True
+    return True    
